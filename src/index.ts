b564a0b1174294299a8b64cc97bcdd44582a54fe
Vatan Aksoy Tezer <vatanaksoytezer@gmail.com>
2024-07-02 13:45:01 +0900

    Conditional runs on backport bot

--- src/index.ts
+++ src/index.ts
@@ -5,32 +5,48 @@ import { getFailureMessage, run } from './run';
 // set environment for APM
 process.env['NODE_ENV'] = 'production-github-action';
 
-run({
-  context,
-  inputs: {
-    accessToken: core.getInput('github_token', {
-      required: true,
-    }),
-    autoBackportLabelPrefix: core.getInput('auto_backport_label_prefix', {
-      required: false,
-    }),
-    repoForkOwner: core.getInput('repo_fork_owner', {
-      required: false,
-    }),
-    addOriginalReviewers: core.getBooleanInput('add_original_reviewers', {
-      required: false,
-    }),
-  },
-})
-  .then((res) => {
-    core.info(`Backport success: ${res.status}`);
-    core.setOutput('Result', res);
-    const failureMessage = getFailureMessage(res);
-    if (failureMessage) {
-      core.setFailed(failureMessage);
-    }
-  })
-  .catch((error) => {
-    core.error(`Backport failure: ${error.message}`);
-    core.setFailed(error.message);
-  });
+// Function to check if the PR contains the label with the given prefix
+const hasAutoBackportLabel = (labels, prefix) => {
+  return labels.some(label => label.name.startsWith(prefix));
+};
+
+// Retrieve inputs
+const autoBackportLabelPrefix = core.getInput('auto_backport_label_prefix', { required: false });
+const prLabels = context.payload.pull_request ? context.payload.pull_request.labels : [];
+
+// Check if the PR is merged
+if (context.payload.pull_request && context.payload.pull_request.merged) {
+  if (autoBackportLabelPrefix && !hasAutoBackportLabel(prLabels, autoBackportLabelPrefix)) {
+    core.info(`Pull request does not contain the label with prefix "${autoBackportLabelPrefix}". Action will not run.`);
+  } else {
+    run({
+      context,
+      inputs: {
+        accessToken: core.getInput('github_token', {
+          required: true,
+        }),
+        autoBackportLabelPrefix,
+        repoForkOwner: core.getInput('repo_fork_owner', {
+          required: false,
+        }),
+        addOriginalReviewers: core.getBooleanInput('add_original_reviewers', {
+          required: false,
+        }),
+      },
+    })
+      .then((res) => {
+        core.info(`Backport success: ${res.status}`);
+        core.setOutput('Result', res);
+        const failureMessage = getFailureMessage(res);
+        if (failureMessage) {
+          core.setFailed(failureMessage);
+        }
+      })
+      .catch((error) => {
+        core.error(`Backport failure: ${error.message}`);
+        core.setFailed(error.message);
+      });
+  }
+} else {
+  core.info('Pull request is not merged. Action will not run.');
+}
